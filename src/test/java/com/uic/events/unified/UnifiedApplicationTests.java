a4c8e74b15cdda4c8179b7f425ea629222dd7cbe
Lokesh Manideep <lbogg@uic.edu>
2024-10-24 10:14:05 -0500

    springboot template

---
+++ src/test/java/com/uic/events/unified/UnifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uic.events.unified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
